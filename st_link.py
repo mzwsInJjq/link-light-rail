93b6e12208ccb3f507329afd8d7a9c781088ac97
J. Chen <140699748+mzwsInJjq@users.noreply.github.com>
2026-04-10 09:53:48 +0800

    Update st_link.py

--- st_link.py
+++ st_link.py
@@ -234,7 +234,7 @@ class TrainGetter():
 
         # compare direction to the selected endpoint
         if direction == self.endpoint_name:
-            pct_distance_along_trip = 1 - (trip_dict["status"]["distanceAlongTrip"] / trip_dict["status"]["totalDistanceAlongTrip"])
+            pct_distance_along_trip = 1 - (trip_dict["status"]["scheduledDistanceAlongTrip"] / trip_dict["status"]["scheduledDistanceAlongTrip"])
         else:
             pct_distance_along_trip = trip_dict["status"]["distanceAlongTrip"] / trip_dict["status"]["totalDistanceAlongTrip"]
 
